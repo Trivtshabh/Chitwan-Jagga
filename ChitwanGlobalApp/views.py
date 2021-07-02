55b638b4cb635f1e2b8520b0814c344d1cfbfe76
Aashish Bhandari <abisamism@gmail.com>
2021-07-02 12:41:47 +0545

    added search

--- ChitwanGlobalApp/views.py
+++ ChitwanGlobalApp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from chitwanjagga.models import Product
+from django.db.models import Q
 
 
 # Create your views here.
 def index(request):
     products = Product.objects.all()
-    print(products[0].name)
+    search_text = request.GET.get('search')
+    if search_text:
+        products = products.filter(Q(name__icontains=search_text)|Q(Description__icontains=search_text )|Q(location__icontains=search_text)) 
     context = {'products':products}
     return render(request, 'index.html', context)
 
