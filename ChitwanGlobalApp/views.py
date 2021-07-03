9f732d5f4334c5701a42102089f7d94153540f2f
Trishabh Vats <monuroy021@gmail.com>
2021-07-03 19:52:07 +0530

    3rd July

--- ChitwanGlobalApp/views.py
+++ ChitwanGlobalApp/views.py
@@ -8,7 +8,7 @@ def index(request):
     products = Product.objects.all()
     search_text = request.GET.get('search')
     if search_text:
-        products = products.filter(Q(name__icontains=search_text)|Q(Description__icontains=search_text )|Q(location__icontains=search_text)) 
+        products = products.filter(Q(name__icontains=search_text)|Q(Description__icontains=search_text)|Q(location__location__icontains=search_text)|Q(category__name__icontains=search_text))
     context = {'products':products}
     return render(request, 'index.html', context)
 
